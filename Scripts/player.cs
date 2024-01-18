6fbe4d5b6fa5da571d3d1691f895f35bce724563
Zack Dowd <zdowd2796@gmail.com>
2024-01-18 15:47:45 -0700

    added gravity

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -4,7 +4,11 @@ using static Godot.GD;
 
 public partial class player : CharacterBody2D
 {
+	[Export]
 	private float speed = 300f;
+	[Export]
+	private float gravity = 15f;
+	private float maxFallVelocity = 1000f;
 	private Vector2 viewportSize;
 	public override void _Ready()
 	{
@@ -17,9 +21,16 @@ public partial class player : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		float direction = Input.GetAxis("move_left", "move_right");
 		var newVelocity = Velocity;
 		var newGlobalPosition = GlobalPosition;
+		// Applying gravity to player
+		newVelocity.Y += gravity;
+		if (newVelocity.Y > maxFallVelocity)
+		{
+			newVelocity.Y = maxFallVelocity;
+		}
+		// Move based off of input directions
+		float direction = Input.GetAxis("move_left", "move_right");
 		if (direction != 0)
 		{
 			newVelocity.X = direction * speed;
@@ -28,7 +39,7 @@ public partial class player : CharacterBody2D
 		{
 			newVelocity.X = Mathf.MoveToward(newVelocity.X, 0, speed);
 		}
-
+		// Teleports player to other side if outside viewport boundaries
 		var teleportMargin = 20;
 		if (GlobalPosition.X > viewportSize.X + teleportMargin)
 		{
