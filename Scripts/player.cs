852d5473bb9b1469e922ba4f3497fc92deed3cb8
Zack Dowd <zdowd2796@gmail.com>
2024-01-18 15:39:22 -0700

    added boundaries

--- Scripts/player.cs
+++ Scripts/player.cs
@@ -5,8 +5,10 @@ using static Godot.GD;
 public partial class player : CharacterBody2D
 {
 	private float speed = 300f;
+	private Vector2 viewportSize;
 	public override void _Ready()
 	{
+		viewportSize = GetViewportRect().Size;
 	}
 
 	public override void _Process(double delta)
@@ -17,6 +19,7 @@ public partial class player : CharacterBody2D
 	{
 		float direction = Input.GetAxis("move_left", "move_right");
 		var newVelocity = Velocity;
+		var newGlobalPosition = GlobalPosition;
 		if (direction != 0)
 		{
 			newVelocity.X = direction * speed;
@@ -26,7 +29,19 @@ public partial class player : CharacterBody2D
 			newVelocity.X = Mathf.MoveToward(newVelocity.X, 0, speed);
 		}
 
+		var teleportMargin = 20;
+		if (GlobalPosition.X > viewportSize.X + teleportMargin)
+		{
+			newGlobalPosition.X = -teleportMargin;
+			GlobalPosition = newGlobalPosition;
+		}
+		else if (GlobalPosition.X < -teleportMargin)
+		{
+			newGlobalPosition.X = viewportSize.X + teleportMargin;
+			GlobalPosition = newGlobalPosition;
+		}
 		Velocity = newVelocity;
 		MoveAndSlide();
+
 	}
 }
